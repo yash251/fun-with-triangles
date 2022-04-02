2a4b7891882ecb5b365b9cc879c454bef80f04f9
Yash Jagtap <jagtapyash2512@gmail.com>
2022-04-03 00:12:36 +0530

    basic layout js

---
+++ index.js
@@ -0,0 +1,20 @@
+const inputs = document.querySelectorAll(".angle-input");
+const checkButton = document.querySelector("#check-button");
+const output = document.querySelector("#output");
+
+function calculateSumOfAngles(angle1, angle2, angle3) {
+    const sumOfAngles = angle1 + angle2 + angle3;
+    return sumOfAngles;
+}
+
+function isTriangle() {
+    const sumOfAngles = calculateSumOfAngles(Number(inputs[0].value), Number(inputs[1].value), Number(inputs[2].value));
+    if(sumOfAngles === 180) {
+        
+    }
+    else {
+
+    }
+}
+
+checkButton.addEventListener('click', isTriangle)
