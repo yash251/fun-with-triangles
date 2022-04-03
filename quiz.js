aef656880f18a37a601d9d0bacb35ce76a1d7510
Yash Jagtap <jagtapyash2512@gmail.com>
2022-04-03 14:45:01 +0530

    raw js

---
+++ quiz.js
@@ -0,0 +1,11 @@
+const quizForm = document.querySelector(".quiz-form");
+const submitAnswer = document.querySelector("#submit-answer");
+const output = document.querySelector("#output")
+
+const correctAnswers = ["90°", "right angles"];
+
+function calculateScore() {
+
+}
+
+submitAnswer.addEventListener('click', calculateScore);
