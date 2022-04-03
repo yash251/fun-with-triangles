1cde0457055d31799f3d989e8b0a8bfd39e15840
Yash Jagtap <jagtapyash2512@gmail.com>
2022-04-03 15:18:06 +0530

    js layout

---
+++ hypotenuse.js
@@ -0,0 +1,9 @@
+const sides = document.querySelectorAll(".side-input");
+const hypotenuseBtn = document.querySelector("#hypotenuse-btn");
+const output = document.querySelector("#output");
+
+function calculateHypotenuse() {
+    console.log("hypotenuse");
+}
+
+hypotenuseBtn.addEventListener('click', calculateHypotenuse);
